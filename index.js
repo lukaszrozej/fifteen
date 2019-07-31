63d1f35b62fe60446d8f7b8a7c11d18a3c91450a
Łukasz Rożej <lukasz.rozej@gmail.com>
2019-07-31 08:50:02 +0200

    Display state

--- index.js
+++ index.js
@@ -1,10 +1,36 @@
-const show = s => {
+const showC = s => {
   console.log(s.slice(0, 4))
   console.log(s.slice(4, 8))
   console.log(s.slice(8, 12))
   console.log(s.slice(12))
 }
 
-let s = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+const createTile = i => {
+  const tile = document.createElement('div')
+  tile.classList.add('tile')
+  tile.textContent = i > 0 ? i : ''
+  return tile
+}
 
-console.log(initialState())
+const board = document.querySelector('.board')
+
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+
+const tiles = numbers.map(createTile)
+
+tiles.forEach(tile => board.appendChild(tile))
+
+const show = state => {
+  state.forEach((tile, index) => {
+    if (tile === 0) return
+    const top = Math.floor(index / 4) * 40
+    const left = (index % 4) * 40
+    console.log(tile, index, top, left)
+    tiles[tile-1].style.top = `${top}px`
+    tiles[tile-1].style.left = `${left}px`
+  })
+}
+
+let s = initialState()
+
+show(s)
